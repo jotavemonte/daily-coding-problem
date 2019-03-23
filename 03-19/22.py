cd611d1e0f4fed43aa94c4a35dac546d3013910d
jotavemonte <jotavemonte@gmail.com>
2019-03-22 23:29:18 -0300

    done

--- 03-19/22.py
+++ 03-19/22.py
@@ -10,11 +10,29 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division?
 '''
 from functools import reduce
+from unittest import TestCase
 
 
-def solution(array):
-  total = reduce(lambda a, b: a*b, array)
-  return list(map(lambda x: total/x if x else total, array))
+def solution(array=[]):
+  number_of_zeros = array.count(0)
+  if number_of_zeros > 1:
+    return [0 for x in array]
+  elif number_of_zeros == 1:
+    is_not_zero = lambda x: x if not 0 else None
+    zero_free_array = filter(is_not_zero, array)
+    total = reduce(lambda a,b: a*b, zero_free_array)
+    return list(map(lambda x: 0 if x else total, array))
+  else:
+    total = reduce(lambda a,b: a*b, array)
+    return list(map(lambda x: total/x if x else total, array))
 
 
-print(solution([3, 2, 1]))
+class Test(TestCase):
+  def test_without_zeros(self):
+    self.assertEqual(solution([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24])
+  
+  def test_with_one_zero(self):
+    self.assertEqual(solution([1,3,0,5]), [0, 0, 15, 0])
+
+  def test_with_two_zeros(self):
+    self.assertEqual(solution([0,0,1,2]), [0, 0, 0, 0])
